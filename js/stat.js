82fd46959442cf78a9aa88fc31b8001ac552ad97
Vadim6895 <54281107+Vadim6895@users.noreply.github.com>
2020-02-03 22:22:31 +0300

    module1-task2

--- js/stat.js
+++ js/stat.js
@@ -11,7 +11,6 @@ var SHADOW_CLOUD_Y = 20;
 var SHADOW_CLOUD_COLOR = 'rgba(0, 0, 0, 0.7)';
 
 var GAP = 10;
-var FONT_GAP = 15;
 var TEXT_WIDTH = 50;
 var BAR_WIDTH = 50;
 var barHeight = CLOUD_HEIGHT - GAP - TEXT_WIDTH - GAP - TEXT_WIDTH;
@@ -19,16 +18,15 @@ var barHeight = CLOUD_HEIGHT - GAP - TEXT_WIDTH - GAP - TEXT_WIDTH;
 var TEXT_COLOR = '#000';
 
 
-
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
-var getMaxElementTime = function(array) {
+var getMaxElementTime = function (array) {
   var MaxElementTime = array[0];
 
-  for(var i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     if (array[i] > MaxElementTime) {
       MaxElementTime = array[i];
     }
@@ -36,7 +34,7 @@ var getMaxElementTime = function(array) {
   return MaxElementTime;
 };
 
- window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
 
   renderCloud(ctx, SHADOW_CLOUD_X, SHADOW_CLOUD_Y, SHADOW_CLOUD_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
@@ -55,22 +53,22 @@ var getMaxElementTime = function(array) {
     ctx.fillText(players[i], (CLOUD_X) * playerInddex + BAR_WIDTH, CLOUD_HEIGHT - GAP);
 
     ctx.fillStyle = 'hsl(235, 87%, ' + HslVariable + '%)';
-    if (i===0) {
+    if (i === 0) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    ctx.fillRect((CLOUD_X) * playerInddex + TEXT_WIDTH, CLOUD_X + (barHeight - (barHeight *times[i]) / maxTime), BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillRect((CLOUD_X) * playerInddex + TEXT_WIDTH, CLOUD_X + (barHeight - (barHeight * times[i]) / maxTime), BAR_WIDTH, (barHeight * times[i]) / maxTime);
 
     ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(Math.floor(times[i]), (CLOUD_X) * playerInddex + BAR_WIDTH, CLOUD_X - (GAP) * 2 +(barHeight - (barHeight *times[i]) / maxTime));
+    ctx.fillText(Math.floor(times[i]), (CLOUD_X) * playerInddex + BAR_WIDTH, CLOUD_X - (GAP) * 2 + (barHeight - (barHeight * times[i]) / maxTime));
 
     var HslVariable = Math.ceil((times[i] / 100 * 2));
     playerInddex++;
   }
-};       /*в целом оно работает), немного промахнулся с общей шириной*/
+};
+/*  в целом оно работает), немного промахнулся с общей шириной*/
 
 
-
-  /*var playerInddex = 1;    Удалю чуть позже, оставил на всякий
+/*  var playerInddex = 1;    Удалю чуть позже, оставил на всякий
   var playerName = "Вы";
   ctx.fillStyle = '#000';
   ctx.fillText(playerName, (CLOUD_X) * playerInddex + BAR_WIDTH, CLOUD_HEIGHT - GAP);
